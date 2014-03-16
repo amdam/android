3b71cff445e60ac99bdaf241853b20ec21a9183b
Adam Curran <amdam7@gmail.com>
2014-03-16 12:28:15 -0600

    Added MediaRouter Listeners and Receiver App ID

--- TestVideo/TestVideo/src/main/java/adam/learn/testvideo/MainActivity.java
+++ TestVideo/TestVideo/src/main/java/adam/learn/testvideo/MainActivity.java
@@ -2,7 +2,9 @@ package adam.learn.testvideo;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.MediaRouteActionProvider;
 import android.support.v7.media.MediaRouteSelector;
 import android.support.v7.media.MediaRouter;
 import android.view.LayoutInflater;
@@ -11,11 +13,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.cast.CastDevice;
 import com.google.android.gms.cast.CastMediaControlIntent;
 
 public class MainActivity extends ActionBarActivity {
+
+    private String RECEIVER_APP_ID = "B4CE97D6";
+
     private MediaRouter mMediaRouter;
     private MediaRouteSelector mMediaRouteSelector;
+    private CastDevice mSelectedDevice;
+    private MyMediaRouterCallback mMediaRouterCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +37,24 @@ public class MainActivity extends ActionBarActivity {
         }
         mMediaRouter = MediaRouter.getInstance(getApplicationContext());
         mMediaRouteSelector = new MediaRouteSelector.Builder()
-                .addControlCategory(CastMediaControlIntent.categoryForCast("YOUR_APPLICATION_ID"))
+                .addControlCategory(CastMediaControlIntent.categoryForCast(RECEIVER_APP_ID))
                 .build();
+
+        mMediaRouterCallback = new MyMediaRouterCallback();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+        super.onCreateOptionsMenu(menu);
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        MenuItem mediaRouteMenuItem = menu.findItem(R.id.media_route_menu_item);
+        MediaRouteActionProvider mediaRouteActionProvider =
+                (MediaRouteActionProvider) MenuItemCompat.getActionProvider(mediaRouteMenuItem);
+        mediaRouteActionProvider.setRouteSelector(mMediaRouteSelector);
+
         return true;
     }
 
@@ -54,6 +70,20 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMediaRouter.addCallback(mMediaRouteSelector, mMediaRouterCallback, MediaRouter.CALLBACK_FLAG_PERFORM_ACTIVE_SCAN);
+    }
+
+    @Override
+    protected void onPause() {
+        if(isFinishing()) {
+            mMediaRouter.removeCallback(mMediaRouterCallback);
+        }
+        super.onPause();
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -67,6 +97,22 @@ public class MainActivity extends ActionBarActivity {
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
+        }
+    }
+
+    private class MyMediaRouterCallback extends MediaRouter.Callback {
+
+        @Override
+        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo info) {
+            mSelectedDevice = CastDevice.getFromBundle(info.getExtras());
+            String routeId = info.getId();
+
+        }
+
+        @Override
+        public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo info) {
+            //teardown();
+            mSelectedDevice = null;
         }
     }
 
